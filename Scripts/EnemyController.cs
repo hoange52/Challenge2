7ae579087c1b5a3db4e6883584d3b72dc6a4875f
hoange52 <43121406+hoange52@users.noreply.github.com>
2018-10-23 21:03:45 -0400

    Add files via upload

---
+++ Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    //access animation
+    private Animator anim;
+
+    //speed of walk
+    public float speed;
+
+    //everything on the ground
+    public LayerMask isGround;
+
+    //empty game object that is child of enemy
+    public Transform wallHitBox;
+
+    //set variable to true when wall is hit
+    private bool wallHit;
+
+    //sets size of hit box of the wall
+    public float wallHitHeight;
+    public float wallHitWidth;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+
+        wallHit = Physics2D.OverlapBox(wallHitBox.position, new Vector2(wallHitWidth, wallHitHeight), 0, isGround);
+        if (wallHit == true)
+        {
+            speed = speed * -1;
+        }
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            Debug.Log("I loved living");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallHitBox.position, new Vector3(wallHitWidth, wallHitHeight, 1));
+    }
+}
